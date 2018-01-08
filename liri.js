82b2d574e0878badf74def8704ae16e9cbbad0fa
Brandon Shapiro <shapyro@Brandons-MacBook-Pro.local>
2018-01-07 20:33:32 -0600

    reading from random.txt file

--- liri.js
+++ liri.js
@@ -1,28 +1,28 @@
 require("dotenv").config();
 var Spotify = require("node-spotify-api");
 var Twitter = require("twitter");
-
+var request = require("request");
 var keys = require("./keys.js");
+var fs = require("fs");
 
 var spotify = new Spotify(keys.spotify);
 var client = new Twitter(keys.twitter);
 
 var command = process.argv[2];
-var songName = process.argv[3];
 
 switch (command) {
   case "my-tweets":
     getTwitter();
     break;
   case "spotify-this-song":
-    // console.log("spotify")
     getSpotify();
     break;
   case "movie-this":
     console.log("movie")
+    getMovie();
     break;
   case "do-what-it-says":
-    console.log("doing things")
+    whatItDo();
     break;
   default:
     console.log("not an option");
@@ -32,7 +32,6 @@ function getTwitter(){
   var params = {screen_name: 'shapyro1'};
   client.get('statuses/user_timeline', params, function(error, tweets, response) {
     if (!error) {
-      // console.log(tweets);
       for (var item of tweets) {
         console.log(item.created_at);
         console.log(item.text);
@@ -44,7 +43,17 @@ function getTwitter(){
 }
 
 function getSpotify(){
-  console.log("spotify");
+
+  var songName = "";
+
+  if (!process.argv[3]){
+    songName = "The Sign";
+  } else{
+    for (var i = 3; i < process.argv.length; i++){
+      songName += " " + process.argv[i];
+    }
+    // console.log(songName.trim());
+  }
 
   spotify.search({ type: 'track', query: songName }, function(err, data) {
     if (err) {
@@ -52,9 +61,13 @@ function getSpotify(){
     } 
    
   var info = data.tracks.items;
-  // console.log(info);
+  console.log("********************");
+  // console.log(info[1]);
+  // console.log(info[1].external_urls.spotify);
    
   for (item of info){
+    console.log("------------------");
+    console.log("song = " + item.name);
     // console.log(item);
     console.log("ALBUM = " + item.album.name);
     
@@ -62,20 +75,66 @@ function getSpotify(){
     for (artist of artists) {
       console.log("artist =  " + artist.name);
     }
+    console.log("url = " + item.external_urls.spotify)
   }
-  // console.log(info.album);
-  // console.log(data.tracks.items.album);
-  // console.log(data.tracks.items.artists);
-  // for (var item of data) {
-  //   console.log(item);
-  // }
+
   });
 
-  // spotify.request('https://api.spotify.com/v1/tracks/7yCPwWs66K8Ba5lFuU2bcx')
+  // extra track search if more info may be needed using trackID
+  // spotify.request('https://api.spotify.com/v1/tracks/6PGoSes0D9eUDeeAafB2As')
   // .then(function(data) {
+  //   console.log("HERE IS THE TRACK INFO:")
   //   console.log(data); 
   // })
   // .catch(function(err) {
   //   console.error('Error occurred: ' + err); 
   // });
+}
+
+function getMovie(){
+  var movieName = "";
+
+  if (!process.argv[3]){
+    movieName = "Mr. Nobody";
+  } else{
+    for (var i = 3; i < process.argv.length; i++){
+      movieName += " " + process.argv[i];
+    }
+    console.log(movieName.trim());
+  }
+
+  var queryUrl = "http://www.omdbapi.com/?t=" + movieName.trim() + "&apikey=trilogy";
+
+  request(queryUrl, function(error, response, body) {
+
+  if (!error && response.statusCode === 200) {
+    console.log(JSON.parse(body));
+    console.log(JSON.parse(body).Title);
+    console.log(JSON.parse(body).Year);
+    console.log(JSON.parse(body).Ratings[0]);
+    console.log(JSON.parse(body).Ratings[1]);
+    console.log(JSON.parse(body).Country);
+    console.log(JSON.parse(body).Language);
+    console.log(JSON.parse(body).Plot);
+    console.log(JSON.parse(body).Actors);
+    // console.log("The movie's rating is: " + JSON.parse(body).year);
+  } else {
+    console.log("Error occured: " + error);
+  }
+});
+}
+
+//  Read from random.txt for botomation
+function whatItDo(){
+  fs.readFile("random.txt", "utf8", function(error, data){
+    if (error){
+      return console.log(error);
+    }
+    var dataArray = data.split(",");
+    // var total = 0;
+    for (key of dataArray){
+      console.log(key);
+    }
+    console.log("something happend");
+  })
 }
