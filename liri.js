6b2ab8bf3b9dfc5b52a78afe8ec68426a850299e
Brandon Shapiro <shapyro@Brandons-MacBook-Pro.local>
2018-01-15 11:59:17 -0600

    added logging

--- liri.js
+++ liri.js
@@ -4,13 +4,17 @@ var Twitter = require("twitter");
 var request = require("request");
 var keys = require("./keys.js");
 var fs = require("fs");
-
+var moment = require('moment');
 var spotify = new Spotify(keys.spotify);
 var client = new Twitter(keys.twitter);
 
 var command = "";
 var songName = "";
 var movieName = "";
+
+//  Today's date for logging
+var dateString = Date.now().toString().substring(0, 10);
+var dateToday = moment.unix(dateString).format("MM/DD/YYYY");
 
 //  condition for command from user input vs do-what-it-says
 if (process.argv[2]) {
@@ -36,6 +40,7 @@ function listen(){
         break;
     default:
       console.log("NOT AN OPTION")
+      fs.appendFileSync('./log.txt', '\n' + dateToday + ' NOT AN OPTION');
       console.log("Options are: \n\"my-tweets\" \n\"spotify-this-song\" \n\"movie-this\" \n\"do-what-it-says\"");
   }
 }
@@ -47,8 +52,13 @@ function getTwitter(){
     if (!error) {
       for (var item of tweets) {
         console.log("-------------");
+        fs.appendFileSync('./log.txt', '\n' + dateToday + ": -------------");
         console.log(item.created_at);
+        fs.appendFileSync('./log.txt', '\n' + dateToday + ': ' + item.created_at);
         console.log('"' + item.text + '"');
+        fs.appendFileSync('./log.txt', '\n' + dateToday + ': "' + item.text + '"');
+
+        // fs.appendFileSync('./log.txt', 'monkey');
       }
     } else {
       console.log(error);
@@ -80,13 +90,18 @@ function getSpotify(){
       var info = data.tracks.items;
       var item = info[0];
       console.log("********************");
+      fs.appendFileSync('./log.txt', '\n' + dateToday + ": ********************");
       console.log("song = " + item.name);
+      fs.appendFileSync('./log.txt', '\n' + dateToday + ": " + item.name);
       console.log("ALBUM = " + item.album.name);
+      fs.appendFileSync('./log.txt', '\n' + dateToday + ": " + item.album.name);
       var artists = item.album.artists;
       for (artist of artists) {
         console.log("artist = " + artist.name);
+        fs.appendFileSync('./log.txt', '\n' + dateToday + ": " + artist.name);
       }
       console.log("url = " + item.external_urls.spotify)
+      fs.appendFileSync('./log.txt', '\n' + dateToday + ": " + item.external_urls.spotify);
     });
   }
 }
@@ -100,11 +115,6 @@ function getMovie(){
     for (var i = 3; i < process.argv.length; i++){
       movieName += " " + process.argv[i];
     }
-    // console.log(movieName.trim());
-  }
-
-  console.log = function() {
-    fs.appendFile
   }
 
   var queryUrl = "http://www.omdbapi.com/?t=" + movieName.trim() + "&apikey=trilogy";
@@ -113,14 +123,23 @@ function getMovie(){
 
     if (!error && response.statusCode === 200) {
         console.log("********************");
+        fs.appendFileSync('./log.txt', '\n' + dateToday + ": ********************");
         console.log("Title: " + JSON.parse(body).Title);
+        fs.appendFileSync('./log.txt', '\n' + dateToday + ": " + JSON.parse(body).Title);
         console.log("Year: " + JSON.parse(body).Year);
+        fs.appendFileSync('./log.txt', '\n' + dateToday + ": " + JSON.parse(body).Year);
         console.log(JSON.parse(body).Ratings[0].Source +": " + JSON.parse(body).Ratings[0].Value);
+        fs.appendFileSync('./log.txt', '\n' + dateToday + ": " + JSON.parse(body).Ratings[0].Value);
         console.log(JSON.parse(body).Ratings[1].Source +": " + JSON.parse(body).Ratings[1].Value);
+        fs.appendFileSync('./log.txt', '\n' + dateToday + ": " + JSON.parse(body).Ratings[1].Value);
         console.log("Country: " + JSON.parse(body).Country);
+        fs.appendFileSync('./log.txt', '\n' + dateToday + ": " + JSON.parse(body).Country);
         console.log("Language: " + JSON.parse(body).Language);
+        fs.appendFileSync('./log.txt', '\n' + dateToday + ": " + JSON.parse(body).Language);
         console.log("Plot: " + JSON.parse(body).Plot);
+        fs.appendFileSync('./log.txt', '\n' + dateToday + ": " + JSON.parse(body).Plot);
         console.log("Actors: " + JSON.parse(body).Actors);
+        fs.appendFileSync('./log.txt', '\n' + dateToday + ": " + JSON.parse(body).Actors);
     } else {
         console.log("Error occured: " + error);
     }
